962b73de34a4d605f733e9157aba1f6762b434fd
guyue <hujb@dtjoy.com>
2023-12-10 12:35:00 +0800

    添加测试类

---
+++ src/test/java/com/jeb/framework/test/TestClass.java
@@ -0,0 +1,15 @@
+package com.jeb.framework.test;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @Date 2023-12-10 12:34
+ * @Author GuYue
+ */
+public class TestClass {
+
+    @Test
+    public void test() {
+        System.out.println("test");
+    }
+}
